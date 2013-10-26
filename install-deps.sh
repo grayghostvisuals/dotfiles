c3171713975a0b33502248a4e171ee6fc6954078
grayghostvisuals <grayghost@grayghostvisuals.com>
2013-10-25 20:06:59 -0400

    Cleaning and Removal of Spottify and VLC Controllers

--- install-deps.sh
+++ install-deps.sh
@@ -5,9 +5,11 @@
 # the `push` command which copies the github compare URL to my clipboard is heaven
 bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
+
 # https://rvm.io
 # rvm for the rubiess
 curl -L https://get.rvm.io | bash -s stable --ruby
+
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
@@ -20,32 +22,20 @@ npm install -g nave
 ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 
-
 # https://github.com/rupa/z
 # z, oh how i love you
 cd ~/code
 git clone https://github.com/rupa/z.git
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
-
 # z binary is already referenced from .bash_profile
-
-
-# https://github.com/dronir/SpotifyControl
-# Spotify Controll Script
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
-
-# https://github.com/jeroenbegyn/VLCControl
-# VLC Controll Script
-cd ~/code
-git clone git://github.com/jeroenbegyn/VLCControl.git
 
 
 # my magic photobooth symlink -> dropbox. I love it.
 # first move Photo Booth folder out of Pictures
 # then start Photo Booth. It'll ask where to put the library.
 # put it in Dropbox/public
+
 
 # now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
 # then you grab public URL and send off your video message in a heartbeat.
@@ -57,4 +47,3 @@ sudo easy_install Pygments
 
 # chrome canary as default
 # on a mac you can set chrome canary as your default inside of Safari preferences :)
-
